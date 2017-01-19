00ee0944a6995cfe1385c343ec03d003b95a3ce7
geoffreymmoore <geoffrey.m.moore@gmail.com>
2017-01-19 15:16:28 -0800

    Initial commit

---
+++ app/src/main/java/com/moore/project1_tempconverter/TempConverterActivity.java
@@ -0,0 +1,15 @@
+package com.moore.project1_tempconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TempConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_converter);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.weather_few_clouds);
+    }
+}
